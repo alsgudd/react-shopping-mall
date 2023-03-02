9a5088aaa95b4659df5c81b56ac69c1286aecb49
peter <peterlee11@naver.com>
2023-03-02 22:59:49 +0900

    Feat : Component 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import Carousel from 'react-bootstrap/Carousel';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <div className="App">
-        안녕하세요 프로젝트 시작합니다.
-    </div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="http://cdn015.negagea.net/wherehouse/2023/main/230227_1.jpg"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="http://cdn015.negagea.net/wherehouse/2023/main/230227_2.jpg"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static.wixstatic.com/media/f20312_f68af1efdf1a48e4a89f60e9f5ce1242~mv2.jpg/v1/fill/w_1903,h_896,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/f20312_f68af1efdf1a48e4a89f60e9f5ce1242~mv2.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
